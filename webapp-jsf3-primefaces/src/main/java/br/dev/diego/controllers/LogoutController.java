2f4c577daeb02bd6af9ff93f6d5a91229dee1b95
Eder Diego de Sousa <me@diego.dev.br>
2023-05-15 22:01:15 -0300

    Autenticação via form login logout

---
+++ webapp-jsf3-primefaces/src/main/java/br/dev/diego/controllers/LogoutController.java
@@ -0,0 +1,24 @@
+package br.dev.diego.controllers;
+
+import jakarta.enterprise.inject.Model;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Model
+public class LogoutController {
+
+    @Inject
+    private FacesContext facesContext;
+
+    public String logout() throws ServletException {
+        HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
+        request.logout();
+        request.getSession().invalidate();
+        facesContext.addMessage(null, new FacesMessage("Sessão encerrada com sucesso!"));
+        return "login.xhtml";
+    }
+
+}
